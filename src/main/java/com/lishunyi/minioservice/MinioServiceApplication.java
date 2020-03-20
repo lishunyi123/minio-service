1b8fc2646618e9949341884b88711933cc0c944f
李顺仪 <lishunyi0109@gmail.com>
2020-03-20 12:13:35 +0800

    :sparkles: 增加配置文件提示

--- src/main/java/com/lishunyi/minioservice/MinioServiceApplication.java
+++ src/main/java/com/lishunyi/minioservice/MinioServiceApplication.java
@@ -1,9 +1,12 @@
 package com.lishunyi.minioservice;
 
+import com.lishunyi.minioservice.config.MinioConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MinioConfiguration.class)
 public class MinioServiceApplication {
 
     public static void main(String[] args) {
