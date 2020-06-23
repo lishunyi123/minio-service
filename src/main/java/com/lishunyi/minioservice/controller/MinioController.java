44a9b05f2c8dfaca45d3b567ade78371fb779567
李顺仪 <lishunyi0109@gmail.com>
2020-06-23 17:00:50 +0800

    :heavy_plus_sign: 增加依赖

--- src/main/java/com/lishunyi/minioservice/controller/MinioController.java
+++ src/main/java/com/lishunyi/minioservice/controller/MinioController.java
@@ -4,7 +4,6 @@ import com.lishunyi.minioservice.model.UploadDTO;
 import com.lishunyi.minioservice.service.MinioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class MinioController {
